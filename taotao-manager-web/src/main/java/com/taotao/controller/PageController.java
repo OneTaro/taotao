cacc3be325948e1553fae7c0daad7135583468dc
wangss <1063889643@qq.com>
2019-03-03 22:35:12 +0800

    add pagecontroller

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,26 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面跳转控制器
+ *
+ * @author wangss
+ * @create 2019-02-23 16:05
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page) {
+        return page;
+    }
+
+}
